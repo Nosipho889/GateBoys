352bf168432fcfab046f15a624dd8558f2de674a
Nosipho889 <54818199+Nosipho889@users.noreply.github.com>
2019-09-19 20:57:50 +0200

    Add files via upload

---
+++ InventoryProduct.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using IdentitySample.Models;
+using System.IO;
+
+namespace Gateboys.Models
+{
+    public class InventoryProduct
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int productId { get; set; }
+
+        [Required]
+        [Display(Name = "Brand Name")]
+        public string brandName { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string productName { get; set; }
+
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string productDescription { get; set; }
+        public byte[] Image { get; set; }
+        public string ImageType { get; set; }
+        [Required]
+        public int categoryId { get; set; }
+        public virtual Category_ Categories { get; set; }
+
+
+        public int catalogueId { get; set; }
+        public virtual Catalogue_ Catalogues { get; set; }
+
+
+        public int SupplierId { get; set; }
+        public virtual Supplier Suppliers { get; set; }
+
+
+
+        [Required]
+        [Display(Name = "Quantity ")]
+        public int quantityOnHand { get; set; }
+
+        [Display(Name = "Unit Price")]
+        public decimal unitPrice { get; set; }
+
+        [Display(Name = "Discount Price")]
+        public decimal DiscountPrice { get; set; }
+
+        [Display(Name = "Total Price")]
+        public decimal totalPrice { get; set; }
+
+        public bool onPromotion { get; set; }
+
+
+
+        public string status { get; set; }
+        [Display(Name = "Quote Quantity")]
+        public int? quantityForQuote { get; set; }
+
+
+        public InventoryProduct()
+        {
+            DiscountPrice = unitPrice;
+        }
+        public void AddProduct(string name, int count, decimal price, string supply)
+        {
+
+
+
+
+
+
+            var Category = db.Category_.Find(6);
+            var Catalogue = db.Catalogue_.Find(1);
+            var Supplier = db.Suppliers.Where(x => x.supplierName == supply).FirstOrDefault();
+            if (Supplier == null)
+            {
+                Supplier = db.Suppliers.Find(1);
+            }
+
+
+            db.Products.Add(new InventoryProduct()
+            {
+
+                productName = name,
+                quantityOnHand = count,
+                quantityForQuote = count,
+                unitPrice = price,
+                brandName = "Enter Brand Name",
+                Categories = Category,
+                Catalogues = Catalogue,
+                Suppliers = Supplier
+
+
+
+
+            });
+            db.SaveChanges();
+        }
+    }
+}
+    
---
+++ InventoryProductsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentitySample.Models;
+using Gateboys.Models;
+using System.IO;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+
+
+
+
+using System.Text;
+using System.Net.Mail;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace Gateboys.Controllers
+{
+    public class InventoryProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: InventoryProducts
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(i => i.Catalogues).Include(i => i.Categories).Include(i => i.Suppliers);
+            return View(products.ToList());
+        }
+
+        // GET: InventoryProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InventoryProduct inventoryProduct = db.Products.Find(id);
+            if (inventoryProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventoryProduct);
+        }
+
+        // GET: InventoryProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.catalogueId = new SelectList(db.Catalogue_, "catalogueId", "catalogueName");
+            ViewBag.categoryId = new SelectList(db.Category_, "categoryId", "categoryName");
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "supplierid", "supplierName");
+            return View();
+        }
+
+        // POST: InventoryProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "productId,brandName,productName,productDescription,Image,ImageType,categoryId,catalogueId,SupplierId,quantityOnHand,unitPrice,DiscountPrice,totalPrice,onPromotion,status,quantityForQuote")] InventoryProduct inventoryProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(inventoryProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.catalogueId = new SelectList(db.Catalogue_, "catalogueId", "catalogueName", inventoryProduct.catalogueId);
+            ViewBag.categoryId = new SelectList(db.Category_, "categoryId", "categoryName", inventoryProduct.categoryId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "supplierid", "supplierName", inventoryProduct.SupplierId);
+            return View(inventoryProduct);
+        }
+
+        // GET: InventoryProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InventoryProduct inventoryProduct = db.Products.Find(id);
+            if (inventoryProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.catalogueId = new SelectList(db.Catalogue_, "catalogueId", "catalogueName", inventoryProduct.catalogueId);
+            ViewBag.categoryId = new SelectList(db.Category_, "categoryId", "categoryName", inventoryProduct.categoryId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "supplierid", "supplierName", inventoryProduct.SupplierId);
+            return View(inventoryProduct);
+        }
+
+        // POST: InventoryProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "productId,brandName,productName,productDescription,Image,ImageType,categoryId,catalogueId,SupplierId,quantityOnHand,unitPrice,DiscountPrice,totalPrice,onPromotion,status,quantityForQuote")] InventoryProduct inventoryProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventoryProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.catalogueId = new SelectList(db.Catalogue_, "catalogueId", "catalogueName", inventoryProduct.catalogueId);
+            ViewBag.categoryId = new SelectList(db.Category_, "categoryId", "categoryName", inventoryProduct.categoryId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "supplierid", "supplierName", inventoryProduct.SupplierId);
+            return View(inventoryProduct);
+        }
+
+        // GET: InventoryProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InventoryProduct inventoryProduct = db.Products.Find(id);
+            if (inventoryProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventoryProduct);
+        }
+
+        // POST: InventoryProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InventoryProduct inventoryProduct = db.Products.Find(id);
+            db.Products.Remove(inventoryProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
